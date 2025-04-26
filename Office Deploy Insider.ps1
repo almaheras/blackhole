6a09083a4763ace593bcbd91b9ee22e167c7ea5b
Almahera <zamkara@disroot.org>
2025-04-26 19:50:57 +0700

    Update Office Deploy Insider.ps1

--- Office Deploy Insider.ps1
+++ Office Deploy Insider.ps1
@@ -12,9 +12,8 @@ $folderName = "Microsoft Office 365 Deployment"
 $tempDir = [System.IO.Path]::GetTempPath()
 $directory = Join-Path -Path $tempDir -ChildPath $folderName
 $odtUrl = "https://raw.githubusercontent.com/almaheras/blackhole/refs/heads/main/setup.exe"
-$configFileName = "Configuration.xml"
-$configPath = Join-Path -Path $directory -ChildPath $configFileName
 $expectedHash = "9530c6156cf8aba25e660b94666158f34ce54c2269af4727486c34b7ac5fb159ec711c9ee98ba5fda673fd29da3204111df52445d64ca5aca077ece79364dd61"
+
 function Get-FileHashValue {
     param([string]$FilePath)
     if (Test-Path $FilePath) {
@@ -23,6 +22,7 @@ function Get-FileHashValue {
     }
     return $null
 }
+
 if (!(Test-Path -Path $directory)) {
     New-Item -ItemType Directory -Path $directory
 }
@@ -44,60 +44,9 @@ if ($shouldDownload) {
 if (-Not (Test-Path $odtPath) -or (Get-Item $odtPath).Extension -ne ".exe") {
     exit
 }
-$configContent = @"
-<Configuration ID="bb0cf90b-e9b1-4351-90ac-e3a71d34f0b3">
-  <Info Description="All licensed products under this subscription account are centrally managed and maintained by Almahera. Users are advised to utilize these services responsibly, adhering to the organization's guidelines and ensuring compliance with all applicable usage policies." />
-  <Add OfficeClientEdition="64" Channel="CurrentPreview">
-    <Product ID="O365ProPlusRetail">
-      <Language ID="id-id" />
-      <Language ID="MatchOS" />
-      <Language ID="en-us" />
-      <ExcludeApp ID="Groove" />
-      <ExcludeApp ID="Lync" />
-      <ExcludeApp ID="OutlookForWindows" />
-    </Product>
-    <Product ID="VisioProRetail">
-      <Language ID="id-id" />
-      <Language ID="MatchOS" />
-      <Language ID="en-us" />
-      <ExcludeApp ID="Groove" />
-      <ExcludeApp ID="Lync" />
-      <ExcludeApp ID="OutlookForWindows" />
-    </Product>
-    <Product ID="ProjectPro2024Volume" PIDKEY="FQQ23-N4YCY-73HQ3-FM9WC-76HF4">
-      <Language ID="id-id" />
-      <Language ID="MatchOS" />
-      <Language ID="en-us" />
-      <ExcludeApp ID="Groove" />
-      <ExcludeApp ID="Lync" />
-      <ExcludeApp ID="OutlookForWindows" />
-    </Product>
-    <Product ID="AccessRuntimeRetail">
-      <Language ID="id-id" />
-      <Language ID="MatchOS" />
-      <Language ID="en-us" />
-    </Product>
-    <Product ID="ProofingTools">
-      <Language ID="en-us" />
-      <Language ID="id-id" />
-    </Product>
-  </Add>
-  <Property Name="SharedComputerLicensing" Value="0" />
-  <Property Name="FORCEAPPSHUTDOWN" Value="TRUE" />
-  <Property Name="DeviceBasedLicensing" Value="0" />
-  <Property Name="SCLCacheOverride" Value="0" />
-  <Property Name="TenantId" Value="bd03d74d-3de5-4fd3-9815-1f25618f5186" />
-  <Property Name="AUTOACTIVATE" Value="1" />
-  <Updates Enabled="TRUE" />
-  <AppSettings>
-    <Setup Name="Company" Value="Almahera" />
-  </AppSettings>
-  <Display Level="Full" AcceptEULA="TRUE" />
-</Configuration>
-"@
-$configContent | Out-File -FilePath $configPath -Encoding UTF8
+
 try {
-    Start-Process -FilePath $odtPath -ArgumentList "/configure $configPath" -Wait
+    Start-Process -FilePath $odtPath -ArgumentList "/configure https://gist.githubusercontent.com/almaheras/2972b39fcf1bd4e2fe2a0f8466a03db6/raw/" -Wait
     reg add "HKCU\Software\Policies\Microsoft\Office\16.0\Common" /v insiderslabbehavior /t REG_DWORD /d 1
 } catch {
 }
